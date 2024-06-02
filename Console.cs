0a0e9424a3856226379e12e0afa0d237b59b5927
EOLindsay <eorleans_lindsay@st.ug.edu.gh>
2024-06-02 23:05:20 +0000

    Triangl Type Identifier

--- Console.cs
+++ Console.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp
             Console.WriteLine("Choose an application to run");
             Console.WriteLine("1. Grade Calculator");
             Console.WriteLine("2. Ticket Price Calculator");
+            Console.WriteLine("3. Triangle Type Identifier")
             int choice = int.Parse(Console.ReadLine());
             switch (choice)
             {
@@ -16,6 +17,9 @@ namespace ConsoleApp
                     break;
                 case 2:
                     TicketPriceCalculator();
+                    break;
+                case 3:
+                    TriangleTypeIdentifier();
                     break;
                 default:
                     Console.WriteLine("Invalid choice. Please try again.");
@@ -55,6 +59,28 @@ namespace ConsoleApp
             else
             {
                 Console.WriteLine("The ticket price is: GHC10");
+            }
+        }
+        static void TriangleTypeIdentifier()
+        {
+            Console.Write("Enter the length of side 1:");
+            double side1 = double.Parse(Console.ReadLine());
+             Console.Write("Enter the length of side 2:");
+            double side2 = double.Parse(Console.ReadLine());
+             Console.Write("Enter the length of side 3:");
+            double side3 = double.Parse(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+            {
+                Console.WriteLine("This triangle is of type Equilateral.");
+            }
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+            {
+                Console.WriteLine("This triangle is of type Isosceles.");
+            }
+            else
+            {
+                Console.WriteLine("This triangle is of type Scalene.")
             }
         }
     }
