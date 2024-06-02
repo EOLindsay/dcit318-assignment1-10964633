cc23d2e9bfd24eb8659ebb4771e119903f1d9fcb
EOLindsay <eorleans_lindsay@st.ug.edu.gh>
2024-06-02 22:48:22 +0000

    Ticket Price Calculator

--- Console.cs
+++ Console.cs
@@ -5,8 +5,27 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose an application to run");
+            Console.WriteLine("1. Grade Calculator");
+            Console.WriteLine("2. Ticket Price Calculator");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    GradeCalculator();
+                    break;
+                case 2:
+                    TicketPriceCalculator();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+        static void GradeCalculator()
+        {  
             Console.Write("Enter a numerical grade between 0 and 100");
-            int grade = int.parse(ConsoleApp.ReadLine());
+            int grade = int.parse(Console.ReadLine());
             if (grade >= 90)
             {
                 Console.WriteLine("The grade is: A");
@@ -24,6 +43,18 @@ namespace ConsoleApp
             }else
             {
                 Console.WriteLine("The grade is: F")
+            }
+        }
+        static void TicketPriceCalculator()
+        {
+            Console.Write("Enter your age: ");
+            if (age >= 65 || age <= 12)
+            {
+                Console.WriteLine("The ticket price is: GHC7");
+            }
+            else
+            {
+                Console.WriteLine("The ticket price is: GHC10");
             }
         }
     }
